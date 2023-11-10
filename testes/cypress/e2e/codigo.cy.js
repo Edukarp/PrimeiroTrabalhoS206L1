0de0a7a5b54783c855f3571a6ef40e0b9cb232f5
InajahaCosta <inajahacostav@gmail.com>
2023-11-10 19:33:37 -0300

    testes finais

--- testes/cypress/e2e/codigo.cy.js
+++ testes/cypress/e2e/codigo.cy.js
@@ -7,6 +7,30 @@ describe('Testando o site do Inatel', () => {
     cy.get('#loginbtn').click()
     cy.get('.alert').should('contain.text', 'Nome de usuário ou senha errados. Por favor tente outra vez.')
 }) 
+  it('Teste de Página Inicial', () => {
+    cy.visit('https://inatel.br/home/');
+    cy.title().should('eq', 'Instituto Nacional de Telecomunicações - Inatel')
+})
+  it('Teste aba cursos', () => {
+    cy.visit('https://inatel.br/vestibular/');
+    cy.contains('Cursos').click();
+    cy.contains('Engenharia Biomédica').click();
+    cy.url().should('include', '/engenharia-biomedica');
+})
+  it('3. Teste de Navegação', () => {
+    cy.visit('https://inatel.br/home/');
+    cy.contains('Conheça o Inatel ').click();
+    cy.url().should('include', '/quem-somos');
+})
+  it('Teste aba Pos', () => {
+    cy.visit('https://inatel.br/home/');
+    cy.contains('Especialização').click();
+})
+  it('Exibe o logotipo do INATEL', () => {
+    cy.visit('https://inatel.br/home/')
+    cy.wait(10000) // Aguarda 10 segundos para o carregamento da página
+    cy.get('.slickComTexto > picture > img')
+})
 
 })
 
